c958dff0fc74f28460ddd7d5f1ed88b1fbc71d8e
Jay Wilson <jayawilson85@gmail.com>
2015-12-06 20:53:00 -0500

    Board positions start at 1

--- ai/random_ai.py
+++ ai/random_ai.py
@@ -3,6 +3,6 @@ from random import randint
 class Random_AI:
     def get_move(self, board):
         # ignore the board and make a random move
-        x = randint(0, board.size - 1)
-        y = randint(0, board.size - 1)
+        x = randint(1, board.size)
+        y = randint(1, board.size)
         return x, y
